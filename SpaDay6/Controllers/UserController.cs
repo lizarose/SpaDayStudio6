f8282980a7855fb4514de65edadfc51b5403c330
Elizabeth Levinson <lizarose266@gmail.com>
2024-06-05 07:15:12 -0500

    Completed Part 2 Studio

---
+++ SpaDay6/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay6.Controllers;
+
+[Route("/user")]
+public class UserController : Controller
+{
+    //Method
+    [HttpGet]
+    [Route("add")]
+    public IActionResult RenderAddUserForm()
+    {
+        return View("Add");//this is the template name Add.cshtml
+    }
+
+[HttpPost("/user")]
+    public IActionResult SubmitAddUserForm(User newUser, string verify) {
+      // add form submission handling code here
+        ViewBag.User = newUser;
+        if(newUser.Password == verify)
+        {
+            // ViewBag.Username = newUser.Username;
+            return View("Index");
+        }
+        else
+        {
+            ViewBag.Error = "Password does not match.";
+            return View("Add");
+        }
+   }
+}
---
+++ SpaDay6/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Net.Http.Headers;
+
+namespace SpaDay6;
+
+public class User
+{
+    //Properties
+    public int Id { get; }
+    static int nextId = 1;
+   public string Username { get; set; }
+   public string Email { get; set; }
+   public string Password { get; set; } 
+   public DateTime Date { get; set; } = DateTime.Now;
+
+   //Constructor
+public User()
+{
+    Id = nextId;
+    nextId++;
+}
+
+public User (string username, string email, string password)
+{
+    Username = username;
+    Email = email;
+    Password = password;
+}
+   //Override
+
+   //Method
+
+}
